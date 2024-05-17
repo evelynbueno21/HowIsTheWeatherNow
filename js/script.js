454b4322258a6b034940dd1d46e8a56fae613655
evelynbueno21 <evelynbueno628@gmail.com>
2024-05-17 11:39:00 -0300

    Project made with HTML, CSS and JavaScript

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// 8738a7a246a92ace7a3f88183c21f168
+
+const apiKey = '8738a7a246a92ace7a3f88183c21f168';
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+
+
+
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search');
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const humidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+
+
+
+
+
+const getWeatherData = async(city) => {
+   const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+   const res = await fetch(apiWeatherURL);
+   const data = await res.json();
+
+   return data;
+}
+
+
+
+const showWeatherData  = async (city) => {
+  const data =  await getWeatherData(city)
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+  humidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  weatherContainer.classList.remove("hide");
+
+
+  document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
+}
+
+
+searchBtn.addEventListener('click', (e) => {
+   e.preventDefault()
+   const city = cityInput.value;
+
+   showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+  
+   if(e.code === "Enter") {
+      const city = e.target.value;
+      
+      showWeatherData(city);
+   }
+});
